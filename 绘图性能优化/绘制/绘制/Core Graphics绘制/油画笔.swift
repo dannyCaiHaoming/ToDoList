045c7069b1174c2773cc929bf8887d37b2b8d08f
danny <dannycaihaoming@gmail.com>
2020-11-02 14:30:41 +0800

    提交内容：
    1.更新尝试代码
    (目前还不清楚如何高效绘制油画笔)
    
    测试建议：

--- 绘图性能优化/绘制/绘制/Core Graphics绘制/油画笔.swift
+++ 绘图性能优化/绘制/绘制/Core Graphics绘制/油画笔.swift
@@ -20,6 +20,7 @@ class OilView: UIView,Input {
     var increaseDrawPoints: [CGPoint] = []
     var bezierPath =  UIBezierPath()
     var cglayer :CGLayer? = nil
+    var fullCGLayer: CGLayer? = nil
     
     lazy var oliPenImage: UIImage? = {
         getOilPenImage()
@@ -104,7 +105,7 @@ class OilView: UIView,Input {
     // 需要将BezierPath变成虚线，取出上面的点，计算出每次多增加的点
     private func getIncreasePoints() {
         
-        let dashPath = self.bezierPath.cgPath.copy(dashingWithPhase: 0, lengths: [OilView.penSize.width*0.5,0])
+        let dashPath = self.bezierPath.cgPath.copy(dashingWithPhase: 0, lengths: [OilView.penSize.width*0.3,0])
         let dashBezierPath = UIBezierPath(cgPath: dashPath)
         
         // 虚线只要moveToPoint
@@ -117,6 +118,9 @@ class OilView: UIView,Input {
                 self.increaseDrawPoints = points
                 self.totalDrawPoints = points
             }else{
+                guard points.count > self.totalDrawPoints.count else {
+                    return
+                }
                 self.increaseDrawPoints = Array(points[(self.totalDrawPoints.count)...])
                 self.totalDrawPoints = points
                 
@@ -142,14 +146,22 @@ class OilView: UIView,Input {
                     let width = max(maxX-minX, OilView.penSize.width)
                     let heigth = max(maxY-minY, OilView.penSize.height)
                     frame = CGRect.init(x: minX, y: minY, width: width, height: heigth)
+                    let transWidth = OilView.penSize.width
+                    frame = frame?.applying(.init(translationX: -transWidth/2, y: -transWidth/2))
                 }else if self.increaseDrawPoints.count == 1,
-                         let point = self.increaseDrawPoints.first {
+                         var point = self.increaseDrawPoints.first {
+                    let width = OilView.penSize.width
+                    point = point.applying(.init(translationX: -width/2, y: -width/2))
                     frame = CGRect(origin: point, size: OilView.penSize)
                 }
-                
+
                 debugPrint("CHLog--  total = \(self.totalDrawPoints.count)")
                 debugPrint("CHLog--  increase = \(self.increaseDrawPoints.count)")
             }
+            
+//            DispatchQueue.main.async {
+//                self.layer.setNeedsDisplay()
+//            }
             
             if let frame_ = frame {
                 debugPrint("CHLog--  frame_ = \(frame_)")
@@ -170,6 +182,11 @@ class OilView: UIView,Input {
         guard let penImage = self.oliPenImage else {
             return
         }
+        
+        var fullCGLayer_ = self.fullCGLayer
+        if fullCGLayer_ == nil {
+            fullCGLayer_ = CGLayer(ctx, size: self.bounds.size, auxiliaryInfo: nil)
+        }
 
         var cglayer_ = self.cglayer
         if cglayer_ == nil {
@@ -186,13 +203,22 @@ class OilView: UIView,Input {
             UIGraphicsPopContext()
         }
 
+        
 
-        
-        
-        self.increaseDrawPoints.forEach { (point) in
-            ctx.draw(cglayer_!, in: .init(origin: point, size: OilView.penSize))
+        guard let fullCGLayer = fullCGLayer_,
+              let fullLayerCtx = fullCGLayer_?.context else {
+            return
         }
         
+        fullLayerCtx.saveGState()
+        let width = OilView.penSize.width
+        fullLayerCtx.translateBy(x: -width/2, y: -width/2)
+        self.increaseDrawPoints.forEach { (point) in
+            fullLayerCtx.draw(cglayer_!, in: .init(origin: point, size: OilView.penSize))
+        }
+        fullLayerCtx.restoreGState()
+        
+        ctx.draw(fullCGLayer, in: self.bounds)
         
         
         
